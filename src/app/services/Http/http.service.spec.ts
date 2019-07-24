9a2a0f0c5e40e7954b133d890e6a3cd04f1a121a
tiagoluizrs <tiagoluizribeirodasilva@gmail.com>
2019-07-24 15:15:45 -0300

    fim

--- src/app/services/Http/http.service.spec.ts
+++ src/app/services/Http/http.service.spec.ts
@@ -17,24 +17,20 @@ describe('HttpService', () => {
   });
   
 
-  it('Verificando requisição ao endpoint de movies', ()=> {
+  /*
+    Apenas para testar se o método get está funcionando.
+  */
+  it('Verificando requisição ao endpoint de plataformas', ()=> {
     let headers = {
       'Content-Type': 'application/json'
     }
-    httpService.get("https://demo3127152.mockable.io/movies", headers).subscribe((data: any) => {
-      let movie = expect(Object.keys(data[0]))
+    httpService.get("http://private-59658d-celulardireto2017.apiary-mock.com/plataformas", headers).subscribe((data: any) => {
+      let platforms = expect(Object.keys(data[0]))
       
-      movie.toContain("id");
-      movie.toContain("titulo_original");
-      movie.toContain("titulo_portugues");
-      movie.toContain("hero3x1");
-      movie.toContain("hero4x1");
-      movie.toContain("poster_p");
-      movie.toContain("poster_m");
-      movie.toContain("poster_g");
-      movie.toContain("poster_gg");
-      movie.toContain("wallpaper");
-      movie.toContain("part_dia");
+      platforms.toContain("plataformas");
+      platforms.toContain("sku");
+      platforms.toContain("nome");
+      platforms.toContain("descricao");
     });
   });
 });
